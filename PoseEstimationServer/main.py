9809a4ffe0c259824f7535737abf0e454e006af8
Patrick <paddy38@web.de>
2022-09-13 13:59:43 +0200

    Added rotation for Left arm

--- PoseEstimationServer/main.py
+++ PoseEstimationServer/main.py
@@ -4,13 +4,41 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from flask import Flask, json
+import math
 
 api = Flask(__name__)
+counter = 0
 
 
 @api.get("/test")
 def get_hello_world():
-    return json.dumps({"LeftUpperArm": [-0.25,0,0], "LeftLowerArm": [-0.5, 0, 0]})
+    global counter
+    counter += .1
+    sin = math.sin(counter) * 0.2
+    cos = math.cos(counter) * 0.2
+    test = {
+        "Hips": [0, .1, 0],
+        "Spine": [0, 0.25, 0],
+        "Neck": [0, 0.5, 0],
+        "Head": [0, .75, 0],
+        "LeftUpperArm": [0, .5, 0],
+        "LeftLowerArm": [-0.25, .5 + sin, cos],
+        "LeftHand": [-0.5, .5 + sin, cos],
+        "LeftFingers": [-0.75, .5 + sin, cos],
+        "RightUpperArm": [0, .5, 0],
+        "RightLowerArm": [0.25, .5, 0],
+        "RightHand": [0.5, .5, 0],
+        "RightFingers": [0.75, .5, 0],
+        "LeftUpperLeg": [-.1, -.1, 0],
+        "LeftLowerLeg": [-.1, -.25, 0],
+        "LeftFoot": [-.1, -.5, 0],
+        "LeftToes": [-.1, -.75, 0],
+        "RightUpperLeg": [.1, -.1, 0],
+        "RightLowerLeg": [.1, -.25, 0],
+        "RightFoot": [.1, -.5, 0],
+        "RightToes": [.1, -.75, 0]
+    }
+    return json.dumps(test)
 
 
 # Press the green button in the gutter to run the script.
