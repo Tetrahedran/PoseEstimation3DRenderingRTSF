11f47f7f0496bdf251b3bb4b2fae8f5d7c054547
Patrick <paddy38@web.de>
2022-10-06 11:10:41 +0200

    Implementation of first approach to pose estimation server

--- PoseEstimationServer/WebCamTestViPNAS.py
+++ PoseEstimationServer/WebCamTestViPNAS.py
@@ -3,63 +3,109 @@ from mmpose.apis.webcam import WebcamExecutor
 from mmpose.apis.webcam.nodes import model_nodes
 from mmcv import Config, DictAction
 import cv2
+import numpy as np
 
 import time
 
-if __name__ == '__main__':
-    model = "vipnas_res50"
-    config_file, checkpoint_file = "", ""
-    if model == "vipnas_mbv3":
-        # ca. 13fps
-        config_file = "checkpoints/topdown_heatmap_vipnas_mbv3_coco_256x192.py"
-        checkpoint_file = "checkpoints/vipnas_mbv3_coco_256x192-7018731a_20211122.pth"
-    elif model == "vipnas_res50":
-        # ca. 21fps
-        config_file = "checkpoints/topdown_heatmap_vipnas_res50_coco_256x192.py"
-        checkpoint_file = "checkpoints/vipnas_res50_coco_256x192-cc43b466_20210624.pth"
-    elif model == "shufflev1":
-        # ca. 24fps
-        config_file = "checkpoints/topdown_heatmap_shufflenetv1_coco_384x288.py"
-        checkpoint_file = "checkpoints/shufflenetv1_coco_384x288-b2930b24_20200804.pth"
-    elif model == "mspn":
-        # ca. 20fps
-        config_file = "checkpoints/topdown_heatmap_mspn50_coco_256x192.py"
-        checkpoint_file = "checkpoints/mspn50_coco_256x192-8fbfb5d0_20201123.pth"
-    elif model == "mobilenetv2":
-        # ca. 29fps
-        config_file = "checkpoints/topdown_heatmap_mobilenetv2_coco_256x192.py"
-        checkpoint_file = "checkpoints/mobilenetv2_coco_256x192-d1e58e7b_20200727.pth"
-    elif model == "litehr":
-        # ca. 6fps
-        config_file = "checkpoints/topdown_heatmap_litehrnet_30_coco_256x192.py"
-        checkpoint_file = "checkpoints/litehrnet30_coco_256x192-4176555b_20210626.pth"
 
-    pose_model = init_pose_model(config_file, checkpoint_file)
+class WebCamPoseInference:
 
-    image_name = "demo/student-euclidean-vector-png-favpng-4T9235DY4cMQYfzR82sPLWnu5.jpg"
+    model = None
 
-    vid = cv2.VideoCapture(0)
+    def __init__(self, model, confidence=0.75):
+        self.model_name = model
+        self.vid = cv2.VideoCapture(0)
+        self.confidence = confidence
 
-    pose_results = None
-    fps = []
-    startTime = time.time()
-    while (time.time() - startTime) < 60:
-        ret, frame = vid.read()
-        start = time.time()
-        pose_results, _ = inference_top_down_pose_model(pose_model, frame)
-        loc_fps = 1 / (time.time() - start)
-        print(loc_fps)
-        fps.append(loc_fps)
-        print(pose_results)
-        for person in pose_results:
+    def start(self):
+        config_file, checkpoint_file = "", ""
+        if self.model_name == "vipnas_mbv3":
+            # ca. 13fps
+            config_file = "checkpoints/topdown_heatmap_vipnas_mbv3_coco_256x192.py"
+            checkpoint_file = "checkpoints/vipnas_mbv3_coco_256x192-7018731a_20211122.pth"
+        elif self.model_name == "vipnas_res50":
+            # ca. 21fps
+            config_file = "checkpoints/topdown_heatmap_vipnas_res50_coco_256x192.py"
+            checkpoint_file = "checkpoints/vipnas_res50_coco_256x192-cc43b466_20210624.pth"
+        elif self.model_name == "shufflev1":
+            # ca. 24fps
+            config_file = "checkpoints/topdown_heatmap_shufflenetv1_coco_384x288.py"
+            checkpoint_file = "checkpoints/shufflenetv1_coco_384x288-b2930b24_20200804.pth"
+        elif self.model_name == "mspn":
+            # ca. 20fps
+            config_file = "checkpoints/topdown_heatmap_mspn50_coco_256x192.py"
+            checkpoint_file = "checkpoints/mspn50_coco_256x192-8fbfb5d0_20201123.pth"
+        elif self.model_name == "mobilenetv2":
+            # ca. 29fps
+            config_file = "checkpoints/topdown_heatmap_mobilenetv2_coco_256x192.py"
+            checkpoint_file = "checkpoints/mobilenetv2_coco_256x192-d1e58e7b_20200727.pth"
+        elif self.model_name == "litehr":
+            # ca. 6fps
+            config_file = "checkpoints/topdown_heatmap_litehrnet_30_coco_256x192.py"
+            checkpoint_file = "checkpoints/litehrnet30_coco_256x192-4176555b_20210626.pth"
+        self.model = init_pose_model(config_file, checkpoint_file)
+
+    def infer(self):
+        if self.model is None:
+            raise Exception("Model not created")
+        ret, frame = self.vid.read()
+        #frame = cv2.flip(frame, 0)
+        pose_results, _ = inference_top_down_pose_model(self.model, frame)
+        self.show_results(frame, pose_results)
+        return self.reformat_results(pose_results, frame.shape)
+
+    def show_results(self, img, results):
+        for person in results:
             for point in person["keypoints"]:
                 confidence = point[2]
-                if confidence > 0.5:
-                    cv2.drawMarker(frame, (int(point[0]), int(point[1])), (0, 255, 0), markerType=cv2.MARKER_CROSS)
-        cv2.imshow("frame", frame)
+                if confidence > self.confidence:
+                    cv2.drawMarker(img, (int(point[0]), int(point[1])), (0, 255, 0), markerType=cv2.MARKER_CROSS)
+        cv2.imshow("frame", img)
         if cv2.waitKey(1) & 0xff == ord("q"):
-            break
-    print(f"Average fps {sum(fps) / len(fps)}")
-    vid.release()
-    cv2.destroyAllWindows()
+            return
+
+    def transform_result(self, raw, x_offset, y_offset, div):
+        #TODO add z-value
+        new = np.array((x_offset - raw[0], -raw[1] + y_offset, 0)) / div
+        return new.tolist()
+
+    def infer_point(self, first, second, x_offset, y_offset, div):
+        calc = self.calculate_point(first, second)
+        return self.transform_result(calc, x_offset, y_offset, div)
+
+    def calculate_point(self, first, second):
+        new = (np.array((first[0], first[1], 0)) + np.array((second[0], second[1], 0))) / 2
+        return new
+
+    def set_extremity(self, points, bone_indices, x_offset, y_offset, div):
+        ret = {}
+        for key in bone_indices:
+            bone_index = bone_indices[key]
+            if points[bone_index][2] >= self.confidence:
+                ret[key] = self.transform_result(points[bone_index], x_offset, y_offset, div)
+        return ret
+
+    def reformat_results(self, pose_results, img_shape):
+        x_offset = img_shape[1] / 2
+        y_offset = img_shape[0] / 2
+        points = pose_results[0]["keypoints"]
+        div = img_shape[1]
+        ret = {}
+
+        # Left Arm Chain
+        ret.update(self.set_extremity(points,
+                                      {"LeftUpperArm": 5, "LeftLowerArm": 7, "LeftHand": 9, "LeftFingers": 9},
+                                      x_offset, y_offset, div))
+        # Right Arm Chain
+        """ret.update(self.set_extremity(points,
+                                      {"RightUpperArm": 6, "RightLowerArm": 8, "RightHand": 10, "RightFingers": 10},
+                                      x_offset, y_offset, div))"""
+        return ret
+
+
+if __name__ == '__main__':
+    wc = WebCamPoseInference("mobilenetv2")
+    wc.start()
+    while True:
+        print(wc.infer())
 
--- PoseEstimationServer/main.py
+++ PoseEstimationServer/main.py
@@ -4,31 +4,49 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from flask import Flask, json
-import math
+from threading import Thread, Lock
+from WebCamTestViPNAS import WebCamPoseInference
+from time import time
 
+
+def perform_pose_estimation():
+    wc = WebCamPoseInference("vipnas_res50", .75)
+    wc.start()
+    fps = []
+    while True:
+        start = time()
+        estimate = wc.infer()
+        end = time()
+        set_pose(estimate)
+        loc_fps = 1 / (end - start)
+        fps.append(loc_fps)
+        if len(fps) >= 10:
+            avg_fps = sum(fps) / len(fps)
+            print(f"Average {avg_fps} fps")
+            fps = []
+
+
+def create_pose_detection_thread():
+    x = Thread(target=perform_pose_estimation, daemon=True)
+    return x
+
+
+# API section
 api = Flask(__name__)
-counter = 0
-
-
-@api.get("/test")
-def get_hello_world():
-    global counter
-    counter += .1
-    sin = math.sin(counter) * 0.2
-    cos = math.cos(counter) * 0.2
-    test = {
+pose_lock = Lock()
+pose = {
         "Hips": [0, .1, 0],
         "Spine": [0, 0.25, 0],
         "Neck": [0, 0.5, 0],
         "Head": [0, .75, 0],
         "LeftUpperArm": [0, .5, 0],
-        "LeftLowerArm": [-0.25, .5 + sin, cos],
-        "LeftHand": [-0.5, .5 + sin, cos],
-        "LeftFingers": [-0.75, .5 + sin, cos],
-        "RightUpperArm": [0, .5 , 0],
-        "RightLowerArm": [0.25, .5 + cos, sin],
-        "RightHand": [0.5, .5 + cos, sin],
-        "RightFingers": [0.75, .5 + cos, sin],
+        "LeftLowerArm": [-0.25, .5, 0],
+        "LeftHand": [-0.5, .5, 0],
+        "LeftFingers": [-0.75, .5, 0],
+        "RightUpperArm": [0, .5, 0],
+        "RightLowerArm": [0.25, .5, 0],
+        "RightHand": [0.5, .5, 0],
+        "RightFingers": [0.75, .5, 0],
         "LeftUpperLeg": [-.1, -.1, 0],
         "LeftLowerLeg": [-.1, -.25, 0],
         "LeftFoot": [-.1, -.5, 0],
@@ -38,11 +56,34 @@ def get_hello_world():
         "RightFoot": [.1, -.5, 0],
         "RightToes": [.1, -.75, 0]
     }
-    return json.dumps(test)
+
+
+def get_pose():
+    pose_lock.acquire()
+    try:
+        return pose
+    finally:
+        pose_lock.release()
+
+
+def set_pose(new_pose):
+    pose_lock.acquire()
+    try:
+        for key in new_pose:
+            pose[key] = new_pose[key]
+    finally:
+        pose_lock.release()
+
+
+@api.get("/test")
+def get_hello_world():
+    return json.dumps(get_pose())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    pose_thread = create_pose_detection_thread()
+    pose_thread.start()
     api.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
