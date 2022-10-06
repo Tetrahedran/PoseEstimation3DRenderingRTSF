727103392000a8253af35808b4ba94ec99fdee5a
Patrick <paddy38@web.de>
2022-10-06 11:09:36 +0200

    Fixed +inf bug

--- SyntheticAvatar/Assets/Scripts/ProxyMover.cs
+++ SyntheticAvatar/Assets/Scripts/ProxyMover.cs
@@ -6,6 +6,8 @@ public class ProxyMover
 {
     private Transform transform;
     private Vector3 prevSmoothedPosition;
+    private int a = 1;
+    private int b = 1;
 
     public ProxyMover(Transform transform)
     {
@@ -16,14 +18,14 @@ public class ProxyMover
     public void move(Vector3 newLocalPos)
     {
         // Setting init value for smoothing
-        if(prevSmoothedPosition == Vector3.positiveInfinity)
+        if(prevSmoothedPosition.Equals(Vector3.positiveInfinity))
         {
             prevSmoothedPosition = newLocalPos;
         }
         // Apply simple smoothing
         else
         {
-            Vector3 smoothedPos = (prevSmoothedPosition + newLocalPos) / 2;
+            Vector3 smoothedPos = (a * prevSmoothedPosition + b * newLocalPos) / (a + b);
             prevSmoothedPosition = smoothedPos;
             transform.localPosition = smoothedPos;
         }
